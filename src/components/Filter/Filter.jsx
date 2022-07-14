a4053f6037f3b9bd31adde9e3d0fe1ba73410fa0
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-07-14 16:39:38 +0300

    Filter update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -20,7 +20,7 @@ export class Filter extends Component {
           name="filter"
           type="search"
           value={value}
-          title="Enter name of the contact to filter data. Filter is case insensitive."
+          title="Enter a name or a family name of the contact to filter data. Filter is case insensitive."
           onChange={this.handleChange}
         />
       </FilterContainer>
